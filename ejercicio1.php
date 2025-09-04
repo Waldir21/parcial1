cd365c69e82d6922e56163c1c88e1a74036429db
Waldir21 <vr19030@ues.edu.sv>
2025-09-04 12:44:32 -0600

    menu personjes de herk

---
+++ ejercicio1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button class "btn1">personajes principales</button>
+    <button class "btn1">villanos</button>
+    <button class "btn1">amigos de shrek</button>
+    <button class "btn1">personajes secundarios</button>
+    <img src="https://drive.google.com/file/d/1Dn_ShLPe4TAjttg7Es3feKFd9082ne3C/view?usp=drive_link" alt="">
+</body>
+</html>
